f4b1dc0e8e54d38f88159fc97df137b7ee362d04
AnJianPeng <penganjian@gmail.com>
2018-12-01 15:45:36 -0500

    sample code to use cache in django

--- src/retrieval/api/models.py
+++ src/retrieval/api/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image_id = models.IntegerField(default=1, )
+    image_id = models.IntegerField(default=1)
     url = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+
+    def get_key(self):
+        return 'image_' + str(self.image_id)
